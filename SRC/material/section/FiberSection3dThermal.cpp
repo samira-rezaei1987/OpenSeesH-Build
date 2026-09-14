04ebfe8716b22ea79eb7bd8de9e89b029198dae0
samira-rezaei1987 <samira-rezaei1987@users.noreply.github.com>
2026-09-14 09:58:38 +0430

    Add setTrial Tmax trace diagnostic

--- SRC/material/section/FiberSection3dThermal.cpp
+++ SRC/material/section/FiberSection3dThermal.cpp
@@ -473,6 +473,15 @@ FiberSection3dThermal::setTrialSectionDeformation (const Vector &deforms)
 	tData(2) = ThermalElongation;
     tData(3) = FiberTempMax;
     iData.setVector(tData);
+	if (FiberTemperature > 570.0 && FiberTemperature < 585.0) {
+        opserr << "\n=== SETTRIAL TMAX TRACE ===\n";
+        opserr << "fiber i          = " << i << endln;
+        opserr << "FiberTemperature = " << FiberTemperature << endln;
+        opserr << "FiberTempMax     = " << FiberTempMax << endln;
+        opserr << "Fiber_TMax[i]    = " << Fiber_TMax[i] << endln;
+        opserr << "tData(3) BEFORE  = " << tData(3) << endln;
+        opserr << "===========================\n";
+  }
     theMat->getVariable("ElongTangent", iData);
     tData = iData.getData();
     tangent = tData(1);
